63ca5d649e0aa33984146753dd8885dc0e9e1c62
Rahath Cherukuri <rahath.cherukuri@gmail.com>
2016-06-28 22:17:08 -0400

    Added API folder and some client code

---
+++ MovieCheckList/API/MVConvenience.swift
@@ -0,0 +1,9 @@
+//
+//  MVConvenience.swift
+//  MovieCheckList
+//
+//  Created by Rahath cherukuri on 6/28/16.
+//  Copyright © 2016 Rahath cherukuri. All rights reserved.
+//
+
+import Foundation
