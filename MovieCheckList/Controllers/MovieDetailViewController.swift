34e9966dcb1a484c2c5d3358627565f2219f3d61
Rahath Cherukuri <rahath.cherukuri@gmail.com>
2016-08-04 01:33:59 -0400

    checking nill values before unwrapping the variables.

--- MovieCheckList/Controllers/MovieDetailViewController.swift
+++ MovieCheckList/Controllers/MovieDetailViewController.swift
@@ -33,11 +33,22 @@ class MovieDetailViewController: UIViewController {
 
         if let movie = movie {
             navigationItem.title = "\(movie.title)"
-
-            movieGenre.text = "Genre: " + movie.genres!
-            movieTime.text = "Time: " + movie.getHoursAndMinutes(Float(movie.runTime!))
-            movieRating.text = "Rating: " + String(format: "%.2f", Double(movie.rating!)) + "/10"
-            movieOverview.text = movie.overview
+            
+            if (movie.runTime != nil) {
+                movieTime.text = "Time: " + movie.getHoursAndMinutes(Float(movie.runTime!))
+            }
+            
+            if (movie.genres != nil) {
+                movieGenre.text = movie.genres
+            }
+            
+            if (movie.rating != nil) {
+                movieRating.text = "Rating: " + String(format: "%.2f", Double(movie.rating!)) + "/10"
+            }
+            
+            if (movie.overview != nil) {
+                movieOverview.text = movie.overview
+            }
             
             var posterSizes = ["w92", "w154", "w185", "w342", "w500", "w780", "original"]
             
@@ -49,7 +60,7 @@ class MovieDetailViewController: UIViewController {
                 stopAndHideSpinner()
             } else if movie.posterPath == nil || movie.posterPath == "" {
                 stopAndHideSpinner()
-                posterImage.image = UIImage(named: "noImage")
+                posterImage.image = UIImage(named: "Film")
             } else {
                 if let posterPath = movie.posterPath {
                     MVClient.sharedInstance.taskForGETImage(posterSizes[5], filePath: posterPath, completionHandler: { (imageData, error) in
